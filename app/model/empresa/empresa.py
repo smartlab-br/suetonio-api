7c87f3208d2759311c5100445ef000bee33e3b4a
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2019-12-18 13:23:10 -0300

    Starting the development of a kafka producer to trigger ingestion of enterprise data.

--- app/model/empresa/empresa.py
+++ app/model/empresa/empresa.py
@@ -1,10 +1,13 @@
 ''' Repository para recuperar informações da CEE '''
 from model.base import BaseModel
 from repository.empresa.empresa import EmpresaRepository
+from kafka import KafkaProducer
+from flask import current_app
 
 #pylint: disable=R0903
 class Empresa(BaseModel):
     ''' Definição do repo '''
+    TOPICS = ['rais', 'catweb', 'autos']
     def __init__(self):
         ''' Construtor '''
         self.repo = EmpresaRepository()
@@ -21,3 +24,15 @@ class Empresa(BaseModel):
         if (only_meta):
             return { 'metadata': metadata, 'dataset': [] }
         return { 'metadata': metadata, 'dataset': dataset }
+
+    def produce(self, cnpj_raiz):
+        kafka_server = f'{current_app.config["KAFKA_HOST"]}:{current_app.config["KAFKA_PORT"]}'
+        # TODO Check namespace for connection
+        # TODO check broker version
+        print(kafka_server)
+        producer = KafkaProducer(bootstrap_servers=kafka_server)
+
+        for t in self.TOPICS:
+            t_name = f'{current_app.config["KAFKA_TOPIC_PREFIX"]}-{t}'
+            print(t_name)
+            producer.send(t_name, cnpj_raiz)
