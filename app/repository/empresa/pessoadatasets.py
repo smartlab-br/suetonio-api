6b5cbcffe4e676410d0789f666ad8ae88d346cdc
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2020-01-14 14:53:34 -0300

    Adding key with date of ingestion.

--- app/repository/empresa/pessoadatasets.py
+++ app/repository/empresa/pessoadatasets.py
@@ -1,4 +1,5 @@
 ''' Repository para recuperar informações de uma empresa '''
+from datetime import datetime
 from repository.base import RedisRepository
 import json
 
@@ -8,9 +9,11 @@ class PessoaDatasetsRepository(RedisRepository):
     REDIS_BASE = 'rx:{}:{}:{}'
     
     def retrieve(self, id_pfpj, ds, pfpj = 'pj'):
-        ''' Obtém o hashset de status de carregamento do REDIS '''
+        ''' Obtém o hashset de status de carregamento dco REDIS '''
         return self.get_dao().hgetall(self.REDIS_BASE.format(pfpj, id_pfpj, ds))
 
     def store_status(self, id_pfpj, ds, competencias, pfpj="pj"):
+        ''' Armazena um registro de status enviado para o kafka '''
         dict_status = {cmp: 'INGESTING' for cmp in competencias}
+        dict_status['when'] = f"{datetime.now():%Y-%m-%d}"
         self.get_dao().hmset(self.REDIS_BASE.format(pfpj, id_pfpj, ds), dict_status)
