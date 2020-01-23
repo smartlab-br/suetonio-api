0b94a9a8da6e1e0977e761f4764685906b3c0f15
Rodrigo Fagundes <rmfagundes@gmail.com>
2020-01-23 10:19:47 -0300

    Adding resource, model and repository. Missing REDIS key definition.

---
+++ app/model/empresa/report.py
@@ -0,0 +1,22 @@
+''' Repository para recuperar informações da CEE '''
+from model.base import BaseModel
+from repository.empresa.report import ReportRepository
+
+#pylint: disable=R0903
+class Report(BaseModel):
+    ''' Definição do repo '''
+    TOPICS = ['rais', 'rfb', 'sisben', 'catweb', 'auto', 'caged', 'rfbsocios', 'rfbparticipacaosocietaria', 'aeronaves', 'renavam']
+
+    def __init__(self):
+        ''' Construtor '''
+        self.repo = ReportRepository()
+
+    def get_repo(self):
+        ''' Garantia de que o repo estará carregado '''
+        if self.repo is None:
+            self.repo = ReportRepository()
+        return self.repo
+
+    def find_report(self, cnpj_raiz):
+        ''' Localiza report pelo CNPJ Raiz '''
+        return self.get_repo().find_report()
