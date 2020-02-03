4781d34da34755e6c88776f7e1250c096b16375e
Rodrigo Fagundes <rmfagundes@gmail.com>
2020-02-03 15:21:24 -0300

    Pylint offenses resolved.

--- app/repository/empresa/empresa.py
+++ app/repository/empresa/empresa.py
@@ -49,117 +49,118 @@ class EmpresaRepository(HBaseRepository):
                     col_pf_name = self.PF_COLUMNS[ds_key]
 
                 if not result[ds_key].empty:
-                    # Filtrar cnpj e id_pf nos datasets pandas
-                    if ('cnpj' in options and options['cnpj'] is not None and
-                            'id_pf' in options and options['id_pf'] is not None and
-                            col_pf_name is not None):
-                        cnpj = options['cnpj']
-                        id_pf = options['id_pf']
-                        if result[ds_key][col_cnpj_name].dtype == 'int64':
-                            cnpj = int(cnpj)
-                        if result[ds_key][col_pf_name].dtype == 'int64':
-                            id_pf = int(id_pf)
-                        result[ds_key] = result[ds_key][
-                            (result[ds_key][col_cnpj_name] == cnpj) &
-                            (result[ds_key][col_pf_name] == id_pf)
-                        ]
-                    # Filtrar apenas cnpj nos datasets pandas
-                    elif 'cnpj' in options and options['cnpj'] is not None:
-                        cnpj = options['cnpj']
-                        if result[ds_key][col_cnpj_name].dtype == 'int64':
-                            cnpj = int(cnpj)
-                        result[ds_key] = result[ds_key][result[ds_key][col_cnpj_name] == cnpj]
-                    # Filtrar apenas id_pf nos datasets pandas
-                    elif ('id_pf' in options and options['id_pf'] is not None and
-                          col_pf_name is not None):
-                        id_pf = options['id_pf']
-                        if result[ds_key][col_pf_name].dtype == 'int64':
-                            id_pf = int(id_pf)
-                        result[ds_key] = result[ds_key][result[ds_key][col_pf_name] == id_pf]
-
-                    if ('perspective' in options and options['perspective'] is not None and
-                            ds_key in self.PERSP_COLUMNS):
-                        result[ds_key] = result[ds_key][
-                            result[ds_key][self.PERSP_COLUMNS[ds_key]] == options['perspective']
-                        ]
-
-                    if not result[ds_key].empty: # Not empty after filters
-                        # Redução de dimensionalidade (simplified)
-                        if 'simplified' in options and options['simplified']:
-                            list_dimred = ['nu_cnpj_cei', 'nu_cpf', 'col_compet']
-                            if ds_key in self.SIMPLE_COLUMNS:
-                                list_dimred = self.SIMPLE_COLUMNS[ds_key]
-                                # Garantir que col_compet sempre estará na lista
-                                if 'col_compet' not in list_dimred:
-                                    list_dimred.append('col_compet')
-                            result[ds_key] = result[ds_key][list_dimred]
-
-                        # Captura de metadados
-                        metadata[ds_key] = {}
-                        metadata[ds_key]['stats'] = json.loads(
-                            result[ds_key].describe(include='all').to_json(orient="index")
-                        )
-
-                        stats_estab = result[ds_key].groupby(col_cnpj_name).describe(include='all')
-                        stats_estab.columns = [
-                            "_".join(col).strip()
-                            for
-                            col
-                            in
-                            stats_estab.columns.values
-                        ]
-                        metadata[ds_key]['stats_estab'] = json.loads(
-                            stats_estab.to_json(orient="index")
-                        )
-
-                        stats_compet = result[ds_key].groupby('col_compet').describe(include='all')
-                        stats_compet.columns = [
-                            "_".join(col).strip() for col in stats_compet.columns.values
-                        ]
-                        metadata[ds_key]['stats_compet'] = json.loads(
-                            stats_compet.to_json(orient="index")
-                        )
-
-                        ## RETIRADO pois a granularidade torna imviável a performance
-                        # metadata['stats_pf'] = result[ds_key][
-                        #     [col_pf_name, 'col_compet']
-                        # ].groupby(col_pf_name).describe(include='all')
-
-                        stats_estab_compet = result[ds_key].groupby(
-                            ['col_compet', col_cnpj_name]
-                        ).describe(include='all')
-                        stats_estab_compet.columns = [
-                            "_".join(col).strip() for col in stats_estab_compet.columns.values
-                        ]
-                        stats_estab_compet = stats_estab_compet.reset_index()
-                        stats_estab_compet['idx'] = stats_estab_compet['col_compet'].apply(str) + \
-                            '_' + stats_estab_compet[col_cnpj_name].apply(str)
-                        stats_estab_compet = stats_estab_compet.set_index('idx')
-                        metadata[ds_key]['stats_estab_compet'] = json.loads(
-                            stats_estab_compet.to_json(orient="index")
-                        )
-
-                        ## RETIRADO pois a granularidade torna imviável a performance
-                        # metadata['stats_pf_compet'] = result[ds_key][
-                        #     [col_pf_name, 'col_compet']
-                        # ].groupby(
-                        #     ['col_compet', col_cnpj_name]
-                        # ).describe(include='all')
-                    else: # Empty after filters
-                        # Captura de metadados
-                        metadata[ds_key] = {}
-                        metadata[ds_key]['stats'] = json.loads(
-                            result[ds_key].describe(include='all').to_json(orient="index")
-                        )
-                else:
-                    # Captura de metadados
-                    metadata[ds_key] = {}
-                    metadata[ds_key]['stats'] = json.loads(
-                        result[ds_key].describe(include='all').to_json(orient="index")
+                    result[ds_key] = self.filter_by_person(
+                        result[ds_key], options, col_cnpj_name, col_pf_name
                     )
+
+                if (not result[ds_key].empty and options.get('perspective') is not None and
+                        ds_key in self.PERSP_COLUMNS):
+                    result[ds_key] = result[ds_key][
+                        result[ds_key][self.PERSP_COLUMNS[ds_key]] == options['perspective']
+                    ]
+
+                # Redução de dimensionalidade (simplified)
+                if not result[ds_key].empty and options.get('simplified'):
+                    list_dimred = ['nu_cnpj_cei', 'nu_cpf', 'col_compet']
+                    if ds_key in self.SIMPLE_COLUMNS:
+                        list_dimred = self.SIMPLE_COLUMNS[ds_key]
+                        # Garantir que col_compet sempre estará na lista
+                        if 'col_compet' not in list_dimred:
+                            list_dimred.append('col_compet')
+                    result[ds_key] = result[ds_key][list_dimred]
+
+                # Captura de metadados
+                metadata[ds_key] = self.get_metadata(result[ds_key], col_cnpj_name)
 
                 # Conversão dos datasets em json
                 result[ds_key] = json.loads(result[ds_key].to_json(orient="records"))
 
             return (result, metadata)
         return (None, None)
+
+    @staticmethod
+    def filter_by_person(dataframe, options, col_cnpj_name, col_pf_name):
+        ''' Filter dataframe by person identification, according to options data '''
+        cnpj = options.get('cnpj')
+        id_pf = options.get('id_pf')
+        # Filtrar cnpj e id_pf nos datasets pandas
+        if cnpj is not None and id_pf is not None and col_pf_name is not None:
+            if dataframe[col_cnpj_name].dtype == 'int64':
+                cnpj = int(cnpj)
+            if dataframe[col_pf_name].dtype == 'int64':
+                id_pf = int(id_pf)
+            dataframe = dataframe[
+                (dataframe[col_cnpj_name] == cnpj) & (dataframe[col_pf_name] == id_pf)
+            ]
+        # Filtrar apenas cnpj nos datasets pandas
+        elif cnpj is not None:
+            if dataframe[col_cnpj_name].dtype == 'int64':
+                cnpj = int(cnpj)
+            dataframe = dataframe[dataframe[col_cnpj_name] == cnpj]
+        # Filtrar apenas id_pf nos datasets pandas
+        elif (id_pf is not None and col_pf_name is not None):
+            if dataframe[col_pf_name].dtype == 'int64':
+                id_pf = int(id_pf)
+            dataframe = dataframe[dataframe[col_pf_name] == id_pf]
+        return dataframe
+
+    @staticmethod
+    def get_metadata(dataframe, col_cnpj_name):
+        ''' Captura metadados de um dataframe '''
+        if dataframe is None:
+            return None
+
+        result = {
+            'stats': json.loads(dataframe.describe(include='all').to_json(orient="index"))
+        }
+
+        if dataframe.empty:
+            return result
+
+        stats_estab = dataframe.groupby(col_cnpj_name).describe(include='all')
+        stats_estab.columns = [
+            "_".join(col).strip()
+            for
+            col
+            in
+            stats_estab.columns.values
+        ]
+        result['stats_estab'] = json.loads(
+            stats_estab.to_json(orient="index")
+        )
+
+        stats_compet = dataframe.groupby('col_compet').describe(include='all')
+        stats_compet.columns = [
+            "_".join(col).strip() for col in stats_compet.columns.values
+        ]
+        result['stats_compet'] = json.loads(
+            stats_compet.to_json(orient="index")
+        )
+
+        ## RETIRADO pois a granularidade torna imviável a performance
+        # metadata['stats_pf'] = dataframe[
+        #     [col_pf_name, 'col_compet']
+        # ].groupby(col_pf_name).describe(include='all')
+
+        stats_estab_compet = dataframe.groupby(
+            ['col_compet', col_cnpj_name]
+        ).describe(include='all')
+        stats_estab_compet.columns = [
+            "_".join(col).strip() for col in stats_estab_compet.columns.values
+        ]
+        stats_estab_compet = stats_estab_compet.reset_index()
+        stats_estab_compet['idx'] = stats_estab_compet['col_compet'].apply(str) + \
+            '_' + stats_estab_compet[col_cnpj_name].apply(str)
+        stats_estab_compet = stats_estab_compet.set_index('idx')
+        result['stats_estab_compet'] = json.loads(
+            stats_estab_compet.to_json(orient="index")
+        )
+
+        ## RETIRADO pois a granularidade torna inviável a performance
+        # metadata['stats_pf_compet'] = dataframe[
+        #     [col_pf_name, 'col_compet']
+        # ].groupby(
+        #     ['col_compet', col_cnpj_name]
+        # ).describe(include='all')
+
+        return result
