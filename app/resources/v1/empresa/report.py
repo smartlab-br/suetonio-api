a010943290cf002c756f0aaa6a3fda681cb3bced
Rodrigo Fagundes <rodrigo.mfagundes@mpt.mp.br>
2020-03-19 09:26:02 -0300

    Adding control statuses.

--- app/resources/v1/empresa/report.py
+++ app/resources/v1/empresa/report.py
@@ -26,7 +26,7 @@ class ReportResource(EmpresaResource):
         if self.is_invalid_id(cnpj_raiz):
             return 400, 'Cnpj raiz inválido (deve ter 8 caracteres exclusivamente numéricos)'
         content = self.get_domain().find_report(cnpj_raiz)
-        rsp_code = {'FAILED': 202, 'PROCESSING': 204, 'NOTFOUND': 202}
+        rsp_code = {'FAILED': 202, 'PROCESSING': 204, 'NOTFOUND': 202, 'RENEWING': 202, 'UNLOCKING': 202}
         if isinstance(content, dict):
             return '', rsp_code[content['status']]
         return Response(content, mimetype='text/html')
@@ -44,7 +44,7 @@ class ReportResource(EmpresaResource):
         if self.is_invalid_id(cnpj_raiz):
             return 400, 'Cnpj raiz inválido (deve ter 8 caracteres exclusivamente numéricos)'
         content = self.get_domain().find_report(cnpj_raiz)
-        rsp_code = {'FAILED': 201, 'PROCESSING': 204, 'NOTFOUND': 201}
+        rsp_code = {'FAILED': 201, 'PROCESSING': 204, 'NOTFOUND': 201, 'RENEWING': 201, 'UNLOCKING': 201}
         if isinstance(content, dict):
             return '', rsp_code[content['status']]
         return Response(content, mimetype='text/html')
