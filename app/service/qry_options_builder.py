4089aaa25f6b1b9d89f8ccc17eb477c4cb38a6c2
Rodrigo Fagundes <rodrigo.mfagundes@mpt.mp.br>
2020-03-20 11:48:39 -0300

    Adjusting behavior for different perspectives on the dataset.

--- app/service/qry_options_builder.py
+++ app/service/qry_options_builder.py
@@ -4,28 +4,34 @@ class QueryOptionsBuilder():
     @classmethod
     def build_options(cls, r_args):
         ''' Constrói as opções da pesquisa '''
+        options = r_args.copy()
+
         categorias = cls.extract_qry_param(r_args, 'categorias')
         if categorias is None:
-            raise ValueError('Categories required')
+            if rules in ['query']:
+                raise ValueError('Categories required')
+        else:
+            options['categorias'] = categorias
 
-        filtros = None
-        if r_args.get('filtros') is not None and r_args.get('filtros'):
+        if r_args.get('filtros') is not None:
             filtros = r_args.get('filtros').replace('\\,', '|')
             filtros = filtros.split(',')
             filtros = [f.replace('|', ',') for f in filtros]
+            options['where'] = filtros
 
-        return {
-            "categorias": categorias,
-            "valor": cls.extract_qry_param(r_args, 'valor'),
-            "agregacao": cls.extract_qry_param(r_args, 'agregacao'),
-            "ordenacao": cls.extract_qry_param(r_args, 'ordenacao'),
-            "where": filtros,
-            "pivot": cls.extract_qry_param(r_args, 'pivot'),
-            "limit": r_args.get('limit'),
-            "offset": r_args.get('offset'),
-            "calcs": cls.extract_qry_param(r_args, 'calcs'),
-            "partition": cls.extract_qry_param(r_args, 'partition')
-        }
+        if r_args.get('theme') is None and rules in ['query']:
+            theme = 'MAIN'
+
+        for k in r_args:
+            if k in ["valor", "agregacao", "ordenacao", "pivot", "calcs", "partition"]:
+                options[k] = cls.extract_qry_param(r_args, k)
+            elif k in ["as_image", "from_viewconf"]:
+                val = False
+                if r_args.get(k, False) == 'S':
+                    val = True
+                options[k] = val
+
+        return options
 
     @staticmethod
     def extract_qry_param(params, param_name):
@@ -47,8 +53,8 @@ class QueryOptionsBuilder():
             options['column'] = r_args['competencia']
         if 'id_pf' in r_args:
             options['id_pf'] = r_args['id_pf']
-        if 'perspectiva' in r_args:
-            options['perspective'] = r_args['perspectiva']
+        if 'perspective' in r_args:
+            options['perspective'] = r_args['perspective']
         if 'only_meta' in r_args and r_args['only_meta'] == 'S':
             options['only_meta'] = True
         if 'reduzido' in r_args and r_args['reduzido'] == 'S':
